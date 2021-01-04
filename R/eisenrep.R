8d1aee8c51236322a4567a5a2779d8e5073fe9a6
LTribelhorn <gitcod@ltribe.ch>
2021-01-04 14:04:13 +0100

    NaN handling, 3rd try

--- R/eisenrep.R
+++ R/eisenrep.R
@@ -16,41 +16,49 @@
 
 eisenrep <- function(data, rating, weigth) {
 
-  if (missing(weigth)){
-    rating <- deparse(substitute(rating))
-    if((sum(data[,rating]=="positiv") + sum(data[,rating]=="negativ") +
-        sum(data[,rating]=="neutral") +  sum(data[,rating]=="ambivalent")) == 0){
-      RepIndex <- NA
-      print(paste("The unweighted Reputationindex (following Eisenegger, 2005) can't be calculated because the denominator is 0.", sep = ""))
-    } else {
-      RepIndex <- ((sum(data[,rating]=="positiv") - sum(data[,rating]=="negativ"))/(sum(data[,rating]=="positiv") + sum(data[,rating]=="negativ")+ sum(data[,rating]=="neutral") + sum(data[,rating]=="ambivalent")))*100
-      print(paste("The unweighted Reputationindex (following Eisenegger, 2005) is ", RepIndex, ".", sep = ""))
+  if(nrow(data) > 1){
+    if (missing(weigth)){
+      rating <- deparse(substitute(rating))
+      if((sum(data[,rating]=="positiv") + sum(data[,rating]=="negativ") +
+          sum(data[,rating]=="neutral") +  sum(data[,rating]=="ambivalent")) == 0){
+        RepIndex <- NA
+        print(paste("The unweighted Reputationindex (following Eisenegger, 2005) can't be calculated because the denominator is 0.", sep = ""))
+      } else {
+        RepIndex <- ((sum(data[,rating]=="positiv") - sum(data[,rating]=="negativ"))/(sum(data[,rating]=="positiv") + sum(data[,rating]=="negativ")+ sum(data[,rating]=="neutral") + sum(data[,rating]=="ambivalent")))*100
+        print(paste("The unweighted Reputationindex (following Eisenegger, 2005) is ", RepIndex, ".", sep = ""))
+      }
+      return(data.frame("RepIndex"=RepIndex))
     }
-    return(data.frame("RepIndex"=RepIndex))
-  }
-  else {
-    rating <- deparse(substitute(rating))
-    weigth <- deparse(substitute(weigth))
-    if((sum(data[,weigth])*(sum(data[,rating]=="positiv") +
-                           sum(data[,rating]=="negativ")+
-                           sum(data[,rating]=="neutral") +
-                           sum(data[,rating]=="ambivalent"))) == 0){
-      gRepIndex <- NA
-      print(paste("The weighted Reputationindex (following Eisenegger, 2005) can't be calculated because the denominator is 0.", sep = ""))
-    } else {
-      gRepIndex <- ((sum(data[,rating]=="positiv")*sum(data[data[,rating]=="positiv", weigth])) - (sum(data[,rating]=="negativ")*sum(data[data[,rating]=="negativ", weigth])))/(sum(data[,weigth])*(sum(data[,rating]=="positiv") + sum(data[,rating]=="negativ")+ sum(data[,rating]=="neutral") + sum(data[,rating]=="ambivalent")))*100
-      print(paste("The weighted Reputationindex (following Eisenegger, 2005) is ", gRepIndex, ".", sep = ""))
-    }
+    else {
+      rating <- deparse(substitute(rating))
+      weigth <- deparse(substitute(weigth))
+      if((sum(data[,weigth])*(sum(data[,rating]=="positiv") +
+                              sum(data[,rating]=="negativ")+
+                              sum(data[,rating]=="neutral") +
+                              sum(data[,rating]=="ambivalent"))) == 0){
+        gRepIndex <- NA
+        print(paste("The weighted Reputationindex (following Eisenegger, 2005) can't be calculated because the denominator is 0.", sep = ""))
+      } else {
+        gRepIndex <- ((sum(data[,rating]=="positiv")*sum(data[data[,rating]=="positiv", weigth])) - (sum(data[,rating]=="negativ")*sum(data[data[,rating]=="negativ", weigth])))/(sum(data[,weigth])*(sum(data[,rating]=="positiv") + sum(data[,rating]=="negativ")+ sum(data[,rating]=="neutral") + sum(data[,rating]=="ambivalent")))*100
+        print(paste("The weighted Reputationindex (following Eisenegger, 2005) is ", gRepIndex, ".", sep = ""))
+      }
 
-    if((sum(data[,rating]=="positiv") + sum(data[,rating]=="negativ") +
-       sum(data[,rating]=="neutral") +  sum(data[,rating]=="ambivalent")) == 0){
-      RepIndex <- NA
-      print(paste("The unweighted Reputationindex (following Eisenegger, 2005) can't be calculated because the denominator is 0.", sep = ""))
-    } else {
-      RepIndex <- ((sum(data[,rating]=="positiv") - sum(data[,rating]=="negativ"))/(sum(data[,rating]=="positiv") + sum(data[,rating]=="negativ")+ sum(data[,rating]=="neutral") + sum(data[,rating]=="ambivalent")))*100
-      print(paste("The unweighted Reputationindex (following Eisenegger, 2005) is ", RepIndex, ".", sep = ""))
-    }
+      if((sum(data[,rating]=="positiv") + sum(data[,rating]=="negativ") +
+          sum(data[,rating]=="neutral") +  sum(data[,rating]=="ambivalent")) == 0){
+        RepIndex <- NA
+        print(paste("The unweighted Reputationindex (following Eisenegger, 2005) can't be calculated because the denominator is 0.", sep = ""))
+      } else {
+        RepIndex <- ((sum(data[,rating]=="positiv") - sum(data[,rating]=="negativ"))/(sum(data[,rating]=="positiv") + sum(data[,rating]=="negativ")+ sum(data[,rating]=="neutral") + sum(data[,rating]=="ambivalent")))*100
+        print(paste("The unweighted Reputationindex (following Eisenegger, 2005) is ", RepIndex, ".", sep = ""))
+      }
 
-    return(data.frame("gRepIndex"=gRepIndex, "RepIndex"=RepIndex))
+      return(data.frame("gRepIndex"=gRepIndex, "RepIndex"=RepIndex))
+    }
+  } else{
+    if (missing(weigth)){
+      return(data.frame("RepIndex"=NA))
+    } else{
+      return(data.frame("gRepIndex"=NA, "RepIndex"=NA))
+    }
   }
 }
